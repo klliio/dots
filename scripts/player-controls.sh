0c4de7f55ff686b9a13e3fbe138849c7a3db38c3
klliio <111129234+klliio@users.noreply.github.com>
2024-01-30 01:34:20 +0000

    Fix controls working when not supposed to
    Fixed mpd being controlled when ncmpcpp is closed by including an if to
    check if it is open.
    
    might find a better solution later

--- scripts/player-controls.sh
+++ scripts/player-controls.sh
@@ -4,11 +4,22 @@
 # 			--[prev/next]
 # 			--[play/pause/toggle]
 
-case "$1" in
-	--next) mpc next > /dev/null ; playerctl next;;
-	--prev) mpc prev > /dev/null ; playerctl previous;;
-	--play) mpc play > /dev/null ; playerctl play;;
-	--pause) mpc pause > /dev/null ; playerctl stop;;
-	--toggle) mpc toggle > /dev/null ; playerctl play-pause;;
-	*) exit 1 ;;
-esac
+if pgrep -x "ncmpcpp" > /dev/null; then
+	case "$1" in
+		--next) mpc next > /dev/null ; playerctl next;;
+		--prev) mpc prev > /dev/null ; playerctl previous;;
+		--play) mpc play > /dev/null ; playerctl play;;
+		--pause) mpc pause > /dev/null ; playerctl stop;;
+		--toggle) mpc toggle > /dev/null ; playerctl play-pause;;
+		*) exit 1 ;;
+	esac
+else
+	case "$1" in
+		--next) playerctl next;;
+		--prev) playerctl previous;;
+		--play) playerctl play;;
+		--pause) playerctl stop;;
+		--toggle) playerctl play-pause;;
+		*) exit 1 ;;
+	esac
+fi
