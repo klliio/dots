d3789577f5e60766f246420f9b47251302369d81
klliio <111129234+klliio@users.noreply.github.com>
2024-01-30 01:34:21 +0000

    Refactor and add status
    Refactored to be easier to add more options in the future.
    Added arguments for getting the status as text or as icons.

--- scripts/player.sh
+++ scripts/player.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-#
+
 # USAGE ./player.sh [args] [length]
 #  ./player.sh --combined 30
+#
+#  arguements:
+#  --status      : just the status
+#  --status-icon : an icon of the status
+#  --icon        : just the icon
+#  --title       : just the title
+#  --combined    : icon and title
 #
 #  Doesn't correctly show spacing with cjk characters
 #  depending on the output location and font. e.g. waybar,
 #  terminal.
-#
+
 export LANG=C; LC_ALL=C
 
 if ! command -v playerctl &> /dev/null; then
@@ -14,33 +21,44 @@ if ! command -v playerctl &> /dev/null; then
     exit 1
 fi
 
-get_playerctl_song() {
-  playerctl metadata --format "{{ artist }} - {{ title }}" 2> /dev/null | sed 's/VEVO//g'
+mpd_client="ncmpcpp"
+mpd_icon="󰎆"
+mpris_icon=""
+status_play="󰐊"
+status_pause="󰏤"
+title() {
+	if pgrep -x "$mpd_client" > /dev/null; then
+		mpc -f %albumartist%\ -\ %title% | sed -n '1p' 2> /dev/null | sed 's/VEVO//g'
+	else
+		playerctl metadata --format "{{ artist }} - {{ title }}" 2> /dev/null | sed 's/VEVO//g'
+	fi
 }
 
-get_mpd_song() {
-	mpc -f %albumartist%\ -\ %title% | sed -n '1p' 2> /dev/null | sed 's/VEVO//g'
+icon() {
+	if pgrep -x "$mpd_client" > /dev/null; then
+		echo "$mpd_icon"
+	else
+		echo "$mpris_icon"
+	fi
 }
 
 mpris="`playerctl status`"
 mpd="`mpc status %state%`"
 if [ "$mpris" == "Playing" ] || [ "$mpd" == "playing" ] ; then
-	if pgrep -x "ncmpcpp" > /dev/null; then
-		title="$(get_mpd_song)"
-		icon="󰎆 "
-	else
-		title="$(get_playerctl_song)"
-		icon=" "
-	fi
-
 	case "$1" in
-		--icon) song="$icon" ;;
-		--title) song="$title" ;;
-		--combined) song="$icon$title" ;;
+		--icon) song="$(icon)" ;;
+		--title) song="$(title)" ;;
+		--combined) song="$(icon) $(title)" ;;
+		--status) song="Playing" ;;
+		--status-icon) song="$status_play" ;;
 		*) exit 1 ;;
 	esac
 else
-	song="Nothing is playing"
+	case "$1" in
+		--status) song="Paused" ;;
+		--status-icon) song="$status_pause" ;;
+		*) song="Nothing is playing" ;;
+	esac
 fi
 
 if [[ -n "$song" ]]; then
