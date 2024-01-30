0577c38bdc1a103f3b5facfb2ee79e438894c8ac
klliio <111129234+klliio@users.noreply.github.com>
2024-01-30 01:34:00 +0000

    Edit appearance & lazy load
    Made the window slightly larger and added transparency.
    
    Made the plugin lazy load in attempt to aid startup times.

--- lua/plugins/FTerm.lua
+++ lua/plugins/FTerm.lua
@@ -1,7 +1,16 @@
 return { -- Floating terminal
   {
     "numToStr/FTerm.nvim",
+    lazy = true,
+    event = 'BufEnter',
     opts = {
+      blend = 30,
+      dimensions = {
+        height = 0.9,
+        width = 0.9,
+        x = 0.5,
+        y = 0.5,
+      },
     },
     config = function(_, opts)
       require("FTerm").setup(opts)
