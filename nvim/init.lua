13ecbeb2365b457017ff98f44f309e3999c89a7f
klliio <111129234+klliio@users.noreply.github.com>
2024-01-30 01:34:21 +0000

    Remove fterm
    Removed fterm as it wasn't being used.

--- nvim/init.lua
+++ nvim/init.lua
@@ -285,9 +285,6 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
--- fterm toggle
-vim.keymap.set('n', '<leader>tt', require('FTerm').toggle, {})
-
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
