6ad79eca141b5bcf6a1e9e25188343d699e64ba6
klliio <111129234+klliio@users.noreply.github.com>
2024-01-30 01:34:20 +0000

    Add wayland support for hidden-windows.sh

--- scripts/hidden-windows.sh
+++ scripts/hidden-windows.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-ids=($(bspc query -N -n .hidden.window)) || exit
-options="$(xtitle "${ids[@]}")"
-id_index="$(<<< "$options" rofi -dmenu -format i)"
-bspc node "${ids[${id_index}]}" -g hidden=off -f
+if echo $XDG_SESSION_TYPE == "wayland"; then
+	ids=($(swaymsg -t get_tree | jq -r 'recurse(.nodes[]) | select(.name == "__i3_scratch").floating_nodes[].id'))
+	names=($(swaymsg -t get_tree | jq -r 'recurse(.nodes[]) | select(.name == "__i3_scratch").floating_nodes[].name'))
+	id_index="$(<<< "$names" rofi -dmenu -format i)"
+	swaymsg "[con_id=$id_index]" focus
+elif echo $XDG_SESSION_TYPE == "tty"; then
+	ids=($(bspc query -N -n .hidden.window)) || exit
+	names="$(xtitle "${ids[@]}")"
+	id_index="$(<<< "$names" rofi -dmenu -format i)"
+	bspc node "${ids[${id_index}]}" -g hidden=off -f
+fi
