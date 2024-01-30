5f8433f06e5fc79b464818e228369e6340f3a6dc
klliio <111129234+klliio@users.noreply.github.com>
2024-01-30 01:34:20 +0000

    Fix rofi showing when no windows are found
    Fixed rofi showing when not windows are found and added a notification
    to indicate that none were found.

--- scripts/hidden-windows.sh
+++ scripts/hidden-windows.sh
@@ -2,15 +2,20 @@
 
 session_type="`echo $XDG_SESSION_TYPE`"
 if [ "$session_type" = "wayland" ] ; then
-	ids=($(swaymsg -t get_tree | jq -r 'recurse(.nodes[]) | select(.name == "__i3_scratch").floating_nodes[].id'))
-	names=$(swaymsg -t get_tree | jq -r 'recurse(.nodes[]) | select(.name == "__i3_scratch").floating_nodes[].name')
-	id_index="$(<<< "$names" rofi -dmenu -p "Hidden" -format i)"
-
-	if [ ! $id_index ]; then
+	if [ $(swaymsg -t get_tree | jq -r 'recurse(.nodes[]) | select(.name == "__i3_scratch").floating_nodes[].name') ] ; then
+		ids=($(swaymsg -t get_tree | jq -r 'recurse(.nodes[]) | select(.name == "__i3_scratch").floating_nodes[].id'))
+		names=$(swaymsg -t get_tree | jq -r 'recurse(.nodes[]) | select(.name == "__i3_scratch").floating_nodes[].name')
+		id_index="$(<<< "$names" rofi -dmenu -p "Hidden" -format i)"
+	
+		if [ ! $id_index ]; then
+			exit 1
+		fi
+	
+		swaymsg "[con_id=${ids[${id_index}]}]" focus
+	else
+        notify-send -a "Hidden Windows" "No windows are in the scratchpad"
 		exit 1
 	fi
-
-	swaymsg "[con_id=${ids[${id_index}]}]" focus
 elif [ "$session_type" = "tty" ] ; then
 	ids=($(bspc query -N -n .hidden.window)) || exit
 	names="$(xtitle "${ids[@]}")"
