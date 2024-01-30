7a9789ad926647a3dd8ad897f845d7a874278f1b
klliio <111129234+klliio@users.noreply.github.com>
2024-01-30 01:34:21 +0000

    Add option to run script
    Added an option to run a script using as case statement as it allows for
    easier expansion in future.

--- scripts/script-manager.sh
+++ scripts/script-manager.sh
@@ -4,7 +4,13 @@ script_dir="/home/klliio/.config/scripts/"
 cmd=$(ls -p /home/klliio/.config/scripts/ | grep -v / | rofi -dmenu -p "Enter a script")
 
 if [ "$cmd" != "" ] ; then
-	eval "cd $script_dir ; alacritty -e nvim $script_dir$cmd"
+	option=$(printf "edit\nrun" | rofi -dmenu -i)
+
+	case "$option" in
+		"edit") eval "cd $script_dir ; alacritty -e nvim $script_dir$cmd" ;;
+		"run") eval "$script_dir$cmd" ;;
+		*) exit 1 ;;
+	esac
 else
 	exit 1
 fi
