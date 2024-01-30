250989b84eed50b4e8408da5061c41a9dcde14d0
klliio <111129234+klliio@users.noreply.github.com>
2024-01-30 01:34:21 +0000

    Add plugins & fix fidget
    Added which-key beacuse i can't remember my bindings
    Add debug.lua from kickstart
    Fix unrecognised field in fidget setup

---
+++ nvim/lua/plugins/which-key.lua
@@ -0,0 +1,88 @@
+return { {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	opts = {
+		plugins = {
+			marks = true,
+			registers = false,
+			spelling = {
+				enabled = false,
+			},
+			presets = {
+				operators = false,
+				motions = true,
+				text_objects = false,
+				windows = true,
+				nav = true,
+				z = false,
+				g = false,
+			},
+
+		},
+		operators = { gc = "Comments" },
+		keylabels = {},
+		motions = {
+			count = true,
+
+		},
+		icons = {
+			breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+			separator = "➜", -- symbol used between a key and it's label
+			group = "+", -- symbol prepended to a group
+		},
+		popup_mappings = {
+			scroll_down = "<c-d>",
+			scroll_up = "<c-u>",
+
+		},
+		window = {
+			border = "single",
+			position = "bottom",
+			margin = { 0, 0, 1, 0 },
+			padding = { 2, 5, 2, 5 },
+			winblend = 0,
+			zindex = 1000,
+
+		},
+		layout = {
+			height = { min = 4, max = 25 },
+			width = { min = 20, max = 50 },
+			spacing = 3,
+			align = "center",
+
+		},
+		ignore_missing = false,
+		hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "^:", "^ ", "^call ", "^lua " },
+		show_help = true,
+		show_keys = true,
+		triggers = "auto",
+		triggers_nowait = {
+			-- marks
+			"`",
+			"'",
+			"g`",
+			"g'",
+			-- registers
+			'"',
+			"<c-r>",
+			-- spelling
+			"z=",
+		},
+		triggers_blacklist = {
+			-- list of mode / prefixes that should never be hooked by WhichKey
+			-- this is mostly relevant for keymaps that start with a native binding
+			i = { "j", "k" },
+			v = { "j", "k" },
+		},
+		-- disable the WhichKey popup for certain buf types and file types.
+		-- Disabled by default for Telescope
+		disable = {
+			buftypes = {},
+			filetypes = {},
+		},
+	}
+} }
