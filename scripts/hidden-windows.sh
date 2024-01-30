669ec963c3a236abc234337d10b940239bc34bbf
klliio <111129234+klliio@users.noreply.github.com>
2024-01-30 01:34:20 +0000

    Add exit case
    Added an exit case to allow exiting wihtout selection.

--- scripts/hidden-windows.sh
+++ scripts/hidden-windows.sh
@@ -5,10 +5,20 @@ if [ "$session_type" = "wayland" ] ; then
 	ids=($(swaymsg -t get_tree | jq -r 'recurse(.nodes[]) | select(.name == "__i3_scratch").floating_nodes[].id'))
 	names=$(swaymsg -t get_tree | jq -r 'recurse(.nodes[]) | select(.name == "__i3_scratch").floating_nodes[].name')
 	id_index="$(<<< "$names" rofi -dmenu -p "Hidden" -format i)"
+
+	if [ ! $id_index ]; then
+		exit 1
+	fi
+
 	swaymsg "[con_id=${ids[${id_index}]}]" focus
 elif [ "$session_type" = "tty" ] ; then
 	ids=($(bspc query -N -n .hidden.window)) || exit
 	names="$(xtitle "${ids[@]}")"
 	id_index="$(<<< "$names" rofi -dmenu -p "Hidden" -format i)"
+
+	if [ ! $id_index ]; then
+		exit 1
+	fi
+
 	bspc node "${ids[${id_index}]}" -g hidden=off -f
 fi
