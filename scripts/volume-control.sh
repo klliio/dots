0d19a066b7261df13478284a5eb1f7d753e36005
klliio <111129234+klliio@users.noreply.github.com>
2024-01-30 01:34:21 +0000

    Add notifications
    Added notifications using bar-rs and mako

--- scripts/volume-control.sh
+++ scripts/volume-control.sh
@@ -10,5 +10,11 @@ if [ "$(wpctl get-volume @DEFAULT_SINK@ | sed 's/^.*\[//;s/\]$//')" != "MUTED" ]
 		--decrease) wpctl set-volume @DEFAULT_SINK@ 5%-;;
 		*) exit 1;;
 	esac
-fi
 
+	id="$(makoctl list | grep -A 26 "Volume Control" | sed 's/.*Volume Control.*id.*data":\([0-9]*\).*urgency.*/\1/g')"
+	if [ -z "$id" ]; then
+		notify-send -a "Volume Control" -i "$HOME/.config/scripts/assets/volume-32x32.png" "$(bar-rs -p $(wpctl get-volume @DEFAULT_SINK@ | sed 's/^.* //;s/\.//') -m 100 -l 27)"
+	else
+		notify-send -r "$id" -a "Volume Control" -i "$HOME/.config/scripts/assets/volume-32x32.png" "$(bar-rs -p $(wpctl get-volume @DEFAULT_SINK@ | sed 's/^.* //;s/\.//') -m 100 -l 27)"
+	fi
+fi
