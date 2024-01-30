37e40970bb631dcd7fa31724ba3ae97b0ba2b8b5
klliio <111129234+klliio@users.noreply.github.com>
2024-01-30 01:34:00 +0000

    Fix wezterm
    Fixed wezterm opacity not working in picom.
    Changed the theme of wezterm to a different version of catppuccin.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -8,9 +8,56 @@ local config = {}
 -- help provide clearer error messages
 if wezterm.config_builder then config = wezterm.config_builder() end
 
-config.color_scheme = 'Catppuccin Mocha'
+config.colors = {
+	background = '#11111b',   -- background
+	foreground = '#cdd6f4',   -- text
+
+	cursor_bg = '#f5e0dc',    -- background when cursor is block
+	cursor_fg = '#11111b',    -- foreground when cursor is block
+	cursor_border = '#cdd6f4', -- cursor colour when bar / block outline
+
+	selection_bg = '#f5e0dc', -- background when text is selected
+	selection_fg = '#11111b', -- text colour when selected
+
+	scrollbar_thumb = '#313244', -- scrollbar
+	split = '#313244',        -- split lines between panes
+
+	ansi = {
+		'#1e1e2e',
+		'#f38ba8',
+		'#a6e3a1',
+		'#f9e2af',
+		'#89b4fa',
+		'#f5c2e7',
+		'#94e2d5',
+		'#bac2de',
+	},
+	brights = {
+		'#1e1e2e',
+		'#f38ba8',
+		'#a6e3a1',
+		'#f9e2af',
+		'#89b4fa',
+		'#f5c2e7',
+		'#94e2d5',
+		'#bac2de',
+	},
+
+	-- ?
+	indexed = { [136] = '#f9e2af' },
+
+	compose_cursor = '#f9e2af', -- show when a leader key is pressed
+
+	copy_mode_active_highlight_bg = { Color = '#f5e0dc' },
+	copy_mode_active_highlight_fg = { Color = '#11111b' },
+	copy_mode_inactive_highlight_bg = { Color = '#bac2de' },
+	copy_mode_inactive_highlight_fg = { Color = '#11111b' },
+}
+
+-- config.color_scheme = 'Catppuccin Mocha'
 config.font = wezterm.font 'Hack Nerd Font Mono'
-config.default_cursor_style = "BlinkingBar"
+config.font_size = 14.0
+config.default_cursor_style = "BlinkingUnderline"
 config.window_decorations = "RESIZE"
 
 config.animation_fps = 1
