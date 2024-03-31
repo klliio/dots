4d9cdcb240dc1fa2fba12ba0e66e078aaaa7e2ab
klliio <111129234+klliio@users.noreply.github.com>
2024-03-31 22:38:59 +0100

    Formatting

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,20 +2,20 @@ local mappings = {}
 
 -- [{{modes}, keymap}] = {"command", {options}}
 mappings.nav = {
-    [{{"n"}, "<space>"}] = { "<Nop>" },
-    [{{"n","v"}, "k"}] = { "v:count == 0 ? 'gk' : 'k'", { expr = true } },
-    [{{"n","v"}, "j"}] = { "v:count == 0 ? 'gj' : 'j'", { expr = true } },
-    [{{"n"}, "<C-Up>"}] = { ":resize -1<CR>", { silent = true, remap = false, desc = "Shrink the Window Horizontally" } },
-    [{{"n"}, "<C-Down>"}] = { ":resize +1<CR>", { silent = true, remap = false, desc = "Grow the Window Horizontally" } },
-    [{{"n"}, "<C-Left>"}] = { ":vertical resize -1<CR>", { silent = true, remap = false, desc = "Shrink the Window Vertically" } },
-    [{{"n"}, "<C-Right>"}] = { ":vertical resize +1<CR>", { silent = true, remap = false, desc = "Grow the Window Vertically" } },
-    [{{"n"}, "<C-k>"}] = { ":wincmd k<CR>", { silent = false, remap = false, desc = "Move up a split" } },
-    [{{"n"}, "<C-j>"}] = { ":wincmd j<CR>", { silent = false, remap = false, desc = "Move down a split" } },
-    [{{"n"}, "<C-h>"}] = { ":wincmd h<CR>", { silent = false, remap = false, desc = "Move left a split" } },
-    [{{"n"}, "<C-l>"}] = { ":wincmd l<CR>", { silent = false, remap = false, desc = "Move right a split" } },
-    [{{"n"}, "<C-w>h"}] = { ":sp", {silent=true, remap=false, desc = "Split Window Horizontally"}},
-    [{{"n"}, "q:"}] = { "<Nop>" },
-    [{{"n"}, "<leader>cc"}] = { function()
+    [{ { "n" }, "<space>" }] = { "<Nop>" },
+    [{ { "n", "v" }, "k" }] = { "v:count == 0 ? 'gk' : 'k'", { expr = true } },
+    [{ { "n", "v" }, "j" }] = { "v:count == 0 ? 'gj' : 'j'", { expr = true } },
+    [{ { "n" }, "<C-Up>" }] = { ":resize -1<CR>", { silent = true, remap = false, desc = "Shrink the Window Horizontally" } },
+    [{ { "n" }, "<C-Down>" }] = { ":resize +1<CR>", { silent = true, remap = false, desc = "Grow the Window Horizontally" } },
+    [{ { "n" }, "<C-Left>" }] = { ":vertical resize -1<CR>", { silent = true, remap = false, desc = "Shrink the Window Vertically" } },
+    [{ { "n" }, "<C-Right>" }] = { ":vertical resize +1<CR>", { silent = true, remap = false, desc = "Grow the Window Vertically" } },
+    [{ { "n" }, "<C-k>" }] = { ":wincmd k<CR>", { silent = false, remap = false, desc = "Move up a split" } },
+    [{ { "n" }, "<C-j>" }] = { ":wincmd j<CR>", { silent = false, remap = false, desc = "Move down a split" } },
+    [{ { "n" }, "<C-h>" }] = { ":wincmd h<CR>", { silent = false, remap = false, desc = "Move left a split" } },
+    [{ { "n" }, "<C-l>" }] = { ":wincmd l<CR>", { silent = false, remap = false, desc = "Move right a split" } },
+    [{ { "n" }, "<C-w>h" }] = { ":sp", { silent = true, remap = false, desc = "Split Window Horizontally" } },
+    [{ { "n" }, "q:" }] = { "<Nop>" },
+    [{ { "n" }, "<leader>cc" }] = { function()
         if vim.wo.cc == "101" then
             vim.opt.colorcolumn = "0"
         else
@@ -25,30 +25,31 @@ mappings.nav = {
 }
 
 mappings.custom = {
-    [{{"n"}, "<leader>yy"}] = { ":%y+<CR><CR>", { silent = true, remap = false, desc = "Yank the Entire Buffer" } },
-    [{{"n"}, "<S-o>"}] = { "^i<CR><Esc>k^i", { silent = true, remap = false, desc = "Insert Line Above" } },
+    [{ { "n" }, "<leader>yy" }] = { ":%y+<CR><CR>", { silent = true, remap = false, desc = "Yank the Entire Buffer" } },
+    [{ { "n" }, "<S-o>" }] = { "^i<CR><Esc>k^i", { silent = true, remap = false, desc = "Insert Line Above" } },
 }
 
 mappings.cybu = {
-    [{{"n"}, "<s-tab>"}] = { "<Plug>(CybuPrev)", { desc = "Move to the Previous Buffer" } }, [{{"n"}, "<tab>"}] = { "<Plug>(CybuNext)", { desc = "Move to the Next Buffer" } },
-    [{{"n"}, "[b"}] = { "<plug>(CybuLastusedPrev)", { desc = "Move to the Previous Buffer" } },
-    [{{"n"}, "]b"}] = { "<plug>(CybuLastusedNext)", { desc = "Move to the Next Buffer" } },
+    [{ { "n" }, "<s-tab>" }] = { "<Plug>(CybuPrev)", { desc = "Move to the Previous Buffer" } },
+    [{ { "n" }, "<tab>" }] = { "<Plug>(CybuNext)", { desc = "Move to the Next Buffer" } },
+    [{ { "n" }, "[b" }] = { "<plug>(CybuLastusedPrev)", { desc = "Move to the Previous Buffer" } },
+    [{ { "n" }, "]b" }] = { "<plug>(CybuLastusedNext)", { desc = "Move to the Next Buffer" } },
 }
 
 mappings.diagnostic = {
-    [{{"n"}, "[d"}] = { vim.diagnostic.goto_prev, { desc = "Go to Previous Diagnostic Message" } },
-    [{{"n"}, "]d"}] = { vim.diagnostic.goto_next, { desc = "Go to Next Diagnostic Message" } },
-    [{{"n"}, "<leader>dm"}] = { vim.diagnostic.open_float, { desc = "Open Floating Diagnostic Message" } },
-    [{{"n"}, "<leader>dl"}] = { vim.diagnostic.setloclist, { desc = "Open Diagnostics List" } },
+    [{ { "n" }, "[d" }] = { vim.diagnostic.goto_prev, { desc = "Go to Previous Diagnostic Message" } },
+    [{ { "n" }, "]d" }] = { vim.diagnostic.goto_next, { desc = "Go to Next Diagnostic Message" } },
+    [{ { "n" }, "<leader>dm" }] = { vim.diagnostic.open_float, { desc = "Open Floating Diagnostic Message" } },
+    [{ { "n" }, "<leader>dl" }] = { vim.diagnostic.setloclist, { desc = "Open Diagnostics List" } },
 }
 
 mappings.leap = {
-    [{{"n", "v"}, "s"}] = { "<Plug>(leap-forward-to)", { remap = true, desc = "Leap Forward" } },
-    [{{"n", "v"}, "<S-s>"}] = { "<Plug>(leap-backward-to)", { remap = true, desc = "Leap Backward" } },
+    [{ { "n", "v" }, "s" }] = { "<Plug>(leap-forward-to)", { remap = true, desc = "Leap Forward" } },
+    [{ { "n", "v" }, "<S-s>" }] = { "<Plug>(leap-backward-to)", { remap = true, desc = "Leap Backward" } },
 }
 
 mappings.glow = {
-    [{{"n"}, "<leader>m"}] = { function()
+    [{ { "n" }, "<leader>m" }] = { function()
         if (vim.bo.filetype == "markdown") then
             vim.cmd("Glow")
         end
@@ -56,32 +57,32 @@ mappings.glow = {
 }
 
 mappings.gitsigns = {
-    [{{"n"}, "[g"}] = { require("gitsigns").prev_hunk, { desc = "Go to Previous Hunk" } },
-    [{{"n"}, "]g"}] = { require("gitsigns").next_hunk, { desc = "Go to Next Hunk" } },
-    [{{"n"}, "<leader>ph"}] = { require("gitsigns").preview_hunk, { desc = "Preview Hunk" } },
+    [{ { "n" }, "[g" }] = { require("gitsigns").prev_hunk, { desc = "Go to Previous Hunk" } },
+    [{ { "n" }, "]g" }] = { require("gitsigns").next_hunk, { desc = "Go to Next Hunk" } },
+    [{ { "n" }, "<leader>ph" }] = { require("gitsigns").preview_hunk, { desc = "Preview Hunk" } },
 }
 
 mappings.debugging = {
-    [{{"n"}, "<F7>"}] = { require("dapui").toggle, { desc = "Toggle DapUi" } },
-    [{{"n"}, "<F6>"}] = { require("dap").terminate, { desc = "Terminate Dap" } },
-    [{{"n"}, "<F5>"}] = { require("dap").continue, { desc = "Continue Dap" } },
-    [{{"n"}, "<F1>"}] = { require("dap").step_into, { desc = "Step Into" } },
-    [{{"n"}, "<F2>"}] = { require("dap").step_over, { desc = "Step Over" } },
-    [{{"n"}, "<F3>"}] = { require("dap").step_out, { desc = "Step Out" } },
-    [{{"n"}, "<leader>b"}] = { require("dap").toggle_breakpoint, { desc = "Toggle Breakpoint" } },
-    [{{"n"}, "<leader>B"}] = { function() require("dap").set_breakpoint(vim.fn.input "Breakpoint condition: ") end, { desc = "Set Breakpoint Condition" } },
+    [{ { "n" }, "<F7>" }] = { require("dapui").toggle, { desc = "Toggle DapUi" } },
+    [{ { "n" }, "<F6>" }] = { require("dap").terminate, { desc = "Terminate Dap" } },
+    [{ { "n" }, "<F5>" }] = { require("dap").continue, { desc = "Continue Dap" } },
+    [{ { "n" }, "<F1>" }] = { require("dap").step_into, { desc = "Step Into" } },
+    [{ { "n" }, "<F2>" }] = { require("dap").step_over, { desc = "Step Over" } },
+    [{ { "n" }, "<F3>" }] = { require("dap").step_out, { desc = "Step Out" } },
+    [{ { "n" }, "<leader>b" }] = { require("dap").toggle_breakpoint, { desc = "Toggle Breakpoint" } },
+    [{ { "n" }, "<leader>B" }] = { function() require("dap").set_breakpoint(vim.fn.input "Breakpoint condition: ") end, { desc = "Set Breakpoint Condition" } },
 }
 
 mappings.telescope = {
-    [{{"n"}, "<leader>?"}] = { require("telescope.builtin").oldfiles, { desc = "Find Recently Opened Files" } },
-    [{{"n"}, "<leader><space>"}] = { require("telescope.builtin").buffers, { desc = "Find Existing Buffers" } },
-    [{{"n"}, "<leader>ff"}] = { require("telescope.builtin").find_files, { desc = "Search Files" } },
-    [{{"n"}, "<leader>gf"}] = { require("telescope.builtin").git_files, { desc = "Search Git Files" } },
-    [{{"n"}, "<leader>sh"}] = { require("telescope.builtin").help_tags, { desc = "Search Help" } },
-    [{{"n"}, "<leader>sw"}] = { require("telescope.builtin").grep_string, { desc = "Search Current Word" } },
-    [{{"n"}, "<leader>sg"}] = { require("telescope.builtin").live_grep, { desc = "Search by Grep" } },
-    [{{"n"}, "<leader>sd"}] = { require("telescope.builtin").diagnostics, { desc = "Search Diagnostics" } },
-    [{{"n"}, "<leader>/"}] = { function()
+    [{ { "n" }, "<leader>?" }] = { require("telescope.builtin").oldfiles, { desc = "Find Recently Opened Files" } },
+    [{ { "n" }, "<leader><space>" }] = { require("telescope.builtin").buffers, { desc = "Find Existing Buffers" } },
+    [{ { "n" }, "<leader>ff" }] = { require("telescope.builtin").find_files, { desc = "Search Files" } },
+    [{ { "n" }, "<leader>gf" }] = { require("telescope.builtin").git_files, { desc = "Search Git Files" } },
+    [{ { "n" }, "<leader>sh" }] = { require("telescope.builtin").help_tags, { desc = "Search Help" } },
+    [{ { "n" }, "<leader>sw" }] = { require("telescope.builtin").grep_string, { desc = "Search Current Word" } },
+    [{ { "n" }, "<leader>sg" }] = { require("telescope.builtin").live_grep, { desc = "Search by Grep" } },
+    [{ { "n" }, "<leader>sd" }] = { require("telescope.builtin").diagnostics, { desc = "Search Diagnostics" } },
+    [{ { "n" }, "<leader>/" }] = { function()
         require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
             previewer = false,
         })
@@ -89,29 +90,29 @@ mappings.telescope = {
 }
 
 mappings.lsp = {
-    [{{"n"}, "<leader>rn"}] = { vim.lsp.buf.rename, { desc = "Rename" } },
-    [{{"n"}, "<leader>fb"}] = { vim.lsp.buf.format, { remap = false, desc = "Format buffer" } },
-    [{{"n"}, "<leader>ca"}] = { vim.lsp.buf.code_action, { desc = "Code Action" } },
-    [{{"n"}, "<leader>td"}] = { vim.lsp.buf.type_definition, { desc = "Type Definition" } },
+    [{ { "n" }, "<leader>rn" }] = { vim.lsp.buf.rename, { desc = "Rename" } },
+    [{ { "n" }, "<leader>fb" }] = { vim.lsp.buf.format, { remap = false, desc = "Format buffer" } },
+    [{ { "n" }, "<leader>ca" }] = { vim.lsp.buf.code_action, { desc = "Code Action" } },
+    [{ { "n" }, "<leader>td" }] = { vim.lsp.buf.type_definition, { desc = "Type Definition" } },
 
     -- Goto
-    [{{"n"}, "gd"}] = { vim.lsp.buf.definition, { desc = "Goto Definition" } },
-    [{{"n"}, "gD"}] = { vim.lsp.buf.declaration, { desc = "Goto Declaration" } },
-    [{{"n"}, "gr"}] = { require("telescope.builtin").lsp_references, { desc = "Goto References" } },
-    [{{"n"}, "gI"}] = { vim.lsp.buf.implementation, { desc = "Goto Implementation" } },
+    [{ { "n" }, "gd" }] = { vim.lsp.buf.definition, { desc = "Goto Definition" } },
+    [{ { "n" }, "gD" }] = { vim.lsp.buf.declaration, { desc = "Goto Declaration" } },
+    [{ { "n" }, "gr" }] = { require("telescope.builtin").lsp_references, { desc = "Goto References" } },
+    [{ { "n" }, "gI" }] = { vim.lsp.buf.implementation, { desc = "Goto Implementation" } },
 
     -- Symbols
-    [{{"n"}, "<leader>ds"}] = { require("telescope.builtin").lsp_document_symbols, { desc = "Document Symbols" } },
-    [{{"n"}, "<leader>ws"}] = { require("telescope.builtin").lsp_dynamic_workspace_symbols, { desc = "Workspace Symbols" } },
+    [{ { "n" }, "<leader>ds" }] = { require("telescope.builtin").lsp_document_symbols, { desc = "Document Symbols" } },
+    [{ { "n" }, "<leader>ws" }] = { require("telescope.builtin").lsp_dynamic_workspace_symbols, { desc = "Workspace Symbols" } },
 
     -- Documentation
-    [{{"n"}, "K"}] = { vim.lsp.buf.hover, { desc = "Hover Documentation" } },
-    [{{"n"}, "<M-k>"}] = { vim.lsp.buf.signature_help, { desc = "Signature Documentation" } },
+    [{ { "n" }, "K" }] = { vim.lsp.buf.hover, { desc = "Hover Documentation" } },
+    [{ { "n" }, "<M-k>" }] = { vim.lsp.buf.signature_help, { desc = "Signature Documentation" } },
 
     -- Workspaces
-    [{{"n"}, "<leader>wa"}] = { vim.lsp.buf.add_workspace_folder, { desc = "Workspace Add Folder" } },
-    [{{"n"}, "<leader>wr"}] = { vim.lsp.buf.remove_workspace_folder, { desc = "Workspace Remove Folder" } },
-    [{{"n"}, "<leader>wl"}] = { function()
+    [{ { "n" }, "<leader>wa" }] = { vim.lsp.buf.add_workspace_folder, { desc = "Workspace Add Folder" } },
+    [{ { "n" }, "<leader>wr" }] = { vim.lsp.buf.remove_workspace_folder, { desc = "Workspace Remove Folder" } },
+    [{ { "n" }, "<leader>wl" }] = { function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, { desc = "Workspace List Folders" } },
 }
@@ -127,7 +128,7 @@ for _, lsp_opts in pairs(mappings.lsp) do
 end
 
 mappings.functions = {
-    lsp = function ()
+    lsp = function()
         for bind, cmd in pairs(mappings.lsp) do
             vim.keymap.set(bind[1], bind[2], cmd[1], cmd[2])
         end
